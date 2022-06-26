41aa92d8df8330075c0a83e7abae1ece47cf5d65
Arcturs <yaninastya2010@yandex.ru>
2022-06-26 14:51:29 +0300

    fixing bugs with connecting

--- src/main/java/ru/vrn/vsu/csf/asashina/yandexproject/configuration/SecurityConfiguration.java
+++ src/main/java/ru/vrn/vsu/csf/asashina/yandexproject/configuration/SecurityConfiguration.java
@@ -12,7 +12,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable();
+        http.cors().disable();
+        http.csrf().disable();
         http.authorizeRequests()
                 .anyRequest().permitAll();
     }
