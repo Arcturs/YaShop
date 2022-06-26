caf8441da76fcf01939eb555efdaa32b8a45e447
Arcturs <yaninastya2010@yandex.ru>
2022-06-26 16:26:42 +0300

    solving problem with swagger

--- src/main/java/ru/vrn/vsu/csf/asashina/yandexproject/configuration/SecurityConfiguration.java
+++ src/main/java/ru/vrn/vsu/csf/asashina/yandexproject/configuration/SecurityConfiguration.java
@@ -1,12 +1,5 @@
 package ru.vrn.vsu.csf.asashina.yandexproject.configuration;
 
-import org.apache.catalina.Context;
-import org.apache.catalina.connector.Connector;
-import org.apache.tomcat.util.descriptor.web.SecurityCollection;
-import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -16,23 +9,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
-    @Bean
-    public ServletWebServerFactory servletContainer() {
-        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
-            @Override
-            protected void postProcessContext(Context context) {
-                SecurityConstraint securityConstraint = new SecurityConstraint();
-                securityConstraint.setUserConstraint("CONFIDENTIAL");
-                SecurityCollection collection = new SecurityCollection();
-                collection.addPattern("/*");
-                securityConstraint.addCollection(collection);
-                context.addConstraint(securityConstraint);
-            }
-        };
-        tomcat.addAdditionalTomcatConnectors(redirectConnector());
-        return tomcat;
-    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -49,14 +25,5 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 "/v3/api-docs",
                 "/webjars/**"
         );
-    }
-
-    private Connector redirectConnector() {
-        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
-        connector.setScheme("http");
-        connector.setPort(80);
-        connector.setSecure(false);
-        connector.setRedirectPort(80);
-        return connector;
     }
 }
--- src/main/java/ru/vrn/vsu/csf/asashina/yandexproject/configuration/SwaggerConfiguration.java
+++ src/main/java/ru/vrn/vsu/csf/asashina/yandexproject/configuration/SwaggerConfiguration.java
@@ -3,9 +3,12 @@ package ru.vrn.vsu.csf.asashina.yandexproject.configuration;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfiguration {
@@ -29,6 +32,8 @@ public class SwaggerConfiguration {
                                 .name("Anastasiya Sashina")
                                 .email("yaninastya2010@yandex.ru")
                         )
-                );
+                )
+                .servers(List.of(new Server().url("/").description("default"),
+                        new Server().url("https://sir-1826.usr.yandex-academy.ru").description("for testing")));
     }
 }
